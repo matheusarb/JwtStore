58f9e53b99d8f1bafd69c04a1f3e2371612a538d
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-02-29 12:06:48 -0300

    EmailVO

---
+++ JwtStore.Core/Contexts/AccountContext/ValueObjects/Email.cs
@@ -0,0 +1,31 @@
+﻿using JwtStore.Core.Contexts.SharedContext.Extensions;
+using JwtStore.Core.Contexts.SharedContext.ValueObjects;
+using System.Text.RegularExpressions;
+
+namespace JwtStore.Core.Contexts.AccountContext.ValueObjects;
+
+public partial class Email : ValueObject
+{
+    private const string Pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+
+    public Email(string address)
+    {
+        if (string.IsNullOrEmpty(address))
+            throw new Exception("Email inválido.");
+        Address = address.Trim().ToLower();
+
+        if(address.Length < 5)
+            throw new Exception("Email inválido.");
+
+        if(!EmailRegex().IsMatch(Address))
+            throw new Exception("Email inválido.");
+
+
+    }
+
+    public string Address { get; }
+    public string Hash => Address.ToBase64();
+
+    [GeneratedRegex(Pattern)]
+    private static partial Regex EmailRegex();
+}
