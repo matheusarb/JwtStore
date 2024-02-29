0e9ac3286e6a63a1382386db37108f8627e05f71
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-02-29 15:19:47 -0300

    add user members

--- JwtStore.Core/Contexts/AccountContext/Entities/User.cs
+++ JwtStore.Core/Contexts/AccountContext/Entities/User.cs
@@ -1,7 +1,11 @@
-﻿using JwtStore.Core.Contexts.SharedContext.Entities;
+﻿using JwtStore.Core.Contexts.AccountContext.ValueObjects;
+using JwtStore.Core.Contexts.SharedContext.Entities;
 
 namespace JwtStore.Core.Contexts.AccountContext.Entities;
 
 public class User : Entity
 {
+    public string Name { get; private set; }
+    public Email Email { get; private set; }
+    
 }
