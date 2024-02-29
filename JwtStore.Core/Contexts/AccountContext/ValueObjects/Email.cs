dca82aed1d70527069babe11aeaa85e534afaa46
Matheus Ribeiro <mat.araujoribeiro@gmail.com>
2024-02-29 14:10:58 -0300

    add Verification to Email

--- JwtStore.Core/Contexts/AccountContext/ValueObjects/Email.cs
+++ JwtStore.Core/Contexts/AccountContext/ValueObjects/Email.cs
@@ -23,12 +23,18 @@ public partial class Email : ValueObject
 
     public string Address { get; }
     public string Hash => Address.ToBase64();
+    public Verification Verification { get; private set; } = new();
 
+    public void ResendVerification()
+        => Verification = new Verification();
+
+    #region Implicit Operators
     public static implicit operator string(Email email)
         => email.ToString();
 
     public static implicit operator Email(string address)
         => new Email(address);
+    #endregion
 
     public override string ToString()
         => Address;
